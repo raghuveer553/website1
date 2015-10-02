4614391cac8685f3c0db35e190cdb2511b2c20ab
Naveen Raghuveer <naveen@streamoid.com>
2015-10-02 17:16:59 +0530

    Initial commit for website#1

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// This is javascript file
+
+var heading = document.querySelector('h1');
+heading.innerHTML = 'Hello world';
+
+document.querySelector('html').onclick = function displayAlert(){
+	alert('Never ever, ever never ');
+}
+
